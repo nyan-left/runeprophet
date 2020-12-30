2146f1c01510676f863b20f349f4c196e1f87e1c
nyan-left <deim.jak@gmail.com>
2020-12-30 22:12:18 +0000

    test lib

--- src/prophet/spec.ts
+++ src/prophet/spec.ts
@@ -1,4 +1,14 @@
 /* eslint-disable camelcase */
+import * as OSRS from 'osrs-trade-stats';
+import axios from 'axios';
+
+OSRS.getFromWiki(OSRS.ITEMS_LIST.Abyssal_whip).then((itemData) => {
+  const requestData = itemData.map((item) => ({ ds: item.dateString, y: item.priceDaily }));
+  axios.post('http://0.0.0.0:8000', requestData).then((res) => {
+    console.log(res.data);
+  });
+});
+
 export interface Root {
   ds: string
   trend: number
